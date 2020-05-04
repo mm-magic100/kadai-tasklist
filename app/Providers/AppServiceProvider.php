a3733940db0fafdd06263a89e5a4bd4637e7aba1
m.magic <mm.maho@icloud.com>
2020-05-04 01:54:57 +0000

    task model

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,9 +13,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+    \Schema::defaultStringLength(191);
+    \URL::forceScheme('https');
     }
-
     /**
      * Register any application services.
      *
